1f031ebeaa48cf18f1e01fc2c5f57956fe72fc2d
Claudia Martin <claudia.alexa.martin@gmail.com>
2022-07-15 05:57:14 -0400

    commit on main

--- Mission-to-Mars/scraping.py
+++ Mission-to-Mars/scraping.py
@@ -12,6 +12,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=False)
 
     news_title, news_paragraph = mars_news(browser)
+    hemisphere_image_urls = hemisphere_scrape(browser)
 
     # Run all scraping functions and store results in dictionary
     data = {
@@ -19,7 +20,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_data": hemisphere_image_urls
     }
     
      # Stop webdriver and return data
@@ -105,6 +107,29 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+# Hemisphere Data:
+
+def hemisphere_scrape(browser):
+    url = 'https://marshemispheres.com/'
+    
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    for hemis in range(4):
+        browser.links.find_by_partial_text('Hemisphere')[hemis].click()
+        html = browser.html
+        hemi_soup = soup(html, 'html.parser')
+        title = hemi_soup.find('h2', class_='title').text
+        img_url = hemi_soup.find('li').a.get('href')
+        hemispheres = {}
+        hemispheres['img_url'] = f'https://marshemispheres.com/{img_url}'
+        hemispheres['title'] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    return hemisphere_image_urls
+    
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
